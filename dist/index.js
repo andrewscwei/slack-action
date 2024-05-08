17cb87e8841c49887fc6b754b67fe01692a63b34
andrewscwei <andrewscwei@users.noreply.github.com>
2024-05-08 23:50:52 +0000

    fix: Handle commit messages from pull requests

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+import * as core from '@actions/core';
+import { sendMessage } from './core/sendMessage.js';
+import { compose } from './utils/compose.js';
+import { getContext } from './utils/context.js';
+import { getInputs } from './utils/inputs.js';
+async function main() {
+    const context = getContext();
+    const inputs = getInputs();
+    const message = compose(context, inputs);
+    core.info('Sending message via Slack API:');
+    core.info(JSON.stringify(message, undefined, 2));
+    return sendMessage(message, {
+        webhookUrl: inputs.webhookUrl,
+    });
+}
+main()
+    .then(res => {
+    core.info(`Slack API response: ${res}`);
+    core.setOutput('response', res);
+})
+    .catch(err => {
+    core.setFailed(`Slack API error: ${err.message}`);
+});
---
+++ dist/utils/context.js
@@ -0,0 +1,38 @@
+import * as github from '@actions/github';
+export function getContext(values) {
+    const actor = values?.actor ?? evalOrThrows(() => github.context.actor, 'actor');
+    const commitMessage = values?.commitMessage ?? getCommitMessage() ?? '<no commit message>';
+    const ref = values?.ref ?? evalOrThrows(() => github.context.ref, 'ref');
+    const repo = values?.repo ?? evalOrThrows(() => `${github.context.repo.owner}/${github.context.repo.repo}`, 'repo');
+    const runId = values?.runId ?? evalOrThrows(() => isNaN(github.context.runId) ? undefined : github.context.runId.toString(), 'run-id');
+    const sha = values?.sha ?? evalOrThrows(() => github.context.sha, 'sha');
+    const workflow = values?.workflow ?? evalOrThrows(() => github.context.workflow, 'workflow');
+    return {
+        actor,
+        commitMessage,
+        ref,
+        repo,
+        runId,
+        sha,
+        workflow,
+    };
+}
+function getCommitMessage() {
+    switch (github.context.eventName) {
+        case 'pull_request':
+            return github.context.payload['pull_request']?.['head']?.['commit']?.['message'];
+        default:
+            return github.context.payload['head_commit']?.['message'];
+    }
+}
+function evalOrThrows(expression, id) {
+    try {
+        const value = expression();
+        if (value === undefined)
+            throw Error(`Expression with ID <${id}> evaluated to undefined value`);
+        return value;
+    }
+    catch (err) {
+        throw Error(`Error evaluating expression with ID <${id}>`);
+    }
+}
